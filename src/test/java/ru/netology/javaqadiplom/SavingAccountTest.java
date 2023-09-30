4ed16eeab89e5731309897427c54f6463916021f
Mirau Pavel <miraupavel@gmail.com>
2023-09-30 17:11:15 +0700

    adding tests

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -15,8 +15,17 @@ public class SavingAccountTest {
         Assertions.assertEquals(2_000, account.getBalance());
     }
 
+//    @Test
+//    public void shouldPlayAboveTheMinimumTwo() {
+//        SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
+//        Boolean expected = false;
+//        Boolean actual = account.pay(8_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+
     @Test
-    public void shouldPlayWithinTheLimit() {   //ошибки нет
+    public void shouldPlayWithinTheLimit() {
         SavingAccount account = new SavingAccount(9_000, 1_000, 10_000, 5);
 
         account.pay(9_000);
@@ -24,9 +33,28 @@ public class SavingAccountTest {
         Assertions.assertEquals(0, account.getBalance());
     }
 
+    @Test
+    public void shouldPlayWithinTheLimitTest() {
+        SavingAccount account = new SavingAccount(9_000, 1_000, 10_000, 5);
+
+        account.pay(6_750);
+
+        Assertions.assertEquals(2_250, account.getBalance());
+    }
+
+
+    @Test
+    public void shouldPlayWithinNegativeLimit() {
+        SavingAccount account = new SavingAccount(9_000, 1_000, 10_000, 5);
+
+        account.pay(-9);
+
+        Assertions.assertEquals(9_000, account.getBalance());
+    }
+
     // тесты на пополнение баланса
     @Test
-    public void shouldAddLessThanMaxBalance() { // Добавляет сумму пополнения, но не считает первоначальный баланс
+    public void shouldAddLessThanMaxBalance() { // Добавляет сумму пополнения, но не учитывает первоначальный баланс
         SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
 
         account.add(3_000);
@@ -36,7 +64,7 @@ public class SavingAccountTest {
     }
 
     @Test
-    public void shouldAddEqualThanMaxBalance() { // Не добавляет сумму пополнения в рамках верхнего лимита
+    public void shouldAddEqualThanMaxBalance() { // Не добавляет сумму пополнения не превышающую рамки верхнего лимита
         SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
 
         account.add(8_000);
@@ -45,7 +73,7 @@ public class SavingAccountTest {
     }
 
     @Test
-    public void shouldAddGreaterThanMaxBalance() { // Всё работает, не добавляет сумму пополнения: попытка выйти за MAXлимит
+    public void shouldAddGreaterThanMaxBalance() {
         SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
 
         account.add(9_000);
@@ -53,18 +81,112 @@ public class SavingAccountTest {
         Assertions.assertEquals(2_000, account.getBalance());
     }
 
-    //тест исключения IllegalArgumentException
     @Test
-    public void shouldAddExceptionThanMaxBalance() { //умирает выводя ошибку.
-        SavingAccount account = new SavingAccount(10_000, 1_000, 10_000, -1);
+    public void shouldAddGreaterZeroBalance() {
+        SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> account.setRate(-1));
+        account.add(0);
 
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddGreaterNegativeBalance() {
+        SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
+
+        account.add(-123);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    //тест исключения IllegalArgumentException
+    @Test //Проверка исключения с процентной ставкой <0
+    public void SavingAccount() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    3_000,
+                    1_000,
+                    10_000,
+                    -5
+            );
+        });
+    }
+
+    @Test //Проверка исключения с начальным балансом > макс баланса
+    public void balanceGreaterThanMaxTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    13_000,
+                    1_000,
+                    10_000,
+                    15
+            );
+        });
+    }
+
+    @Test //Проверка исключения с отрицательным минимальным балансом
+    public void balancenegativeMinTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    500,
+                    -1_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+    @Test //Проверка исключения с отрицательным максимальным балансом
+    public void balanceLessThanMaxTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    500,
+                    1_000,
+                    -10_000,
+                    5
+            );
+        });
+    }
+
+    @Test //Проверка исключения с минимальным балансом > макс баланса
+    public void balanceMinGreatMaxTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    3_000,
+                    10_000,
+                    1_000,
+                    15
+            );
+        });
+    }
+
+    @Test //Проверка исключения с начальным балансом < минимального баланса
+    public void initialBalanceMinNegativeTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    -3,
+                    0,
+                    10_000,
+                    15
+            );
+        });
+    }
+
+    @Test //Проверка исключения с начальным балансом > максимального баланса
+    public void initialBalanceGreatMaxBalanceTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    30_000,
+                    1_000,
+                    10_000,
+                    15
+            );
+        });
     }
 
     //Операция расчёта процентов на остаток счёта
     @Test
-    public void percenrageShouldBeCalculated() { //тест проходит верно
+    public void percenrageShouldBeCalculated() {
         SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 15);
 
         account.yearChange();
@@ -75,12 +197,34 @@ public class SavingAccountTest {
     }
 
     @Test
-    public void percenrageShouldBeCalculated1() { //тест проходит верно
+    public void percenrageShouldBeCalculated1() {
         SavingAccount account = new SavingAccount(10_000, 1_000, 10_000, 1);
 
         account.yearChange();
         int actual = account.yearChange();
         int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void percenrageShouldBeCalculated2() {
+        SavingAccount account = new SavingAccount(10_000, 1_000, 10_000, 0);
+
+        account.yearChange();
+        int actual = account.yearChange();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void percenrageShouldBeCalculated3() {
+        SavingAccount account = new SavingAccount(7_643, 1_000, 10_000, 10);
+
+        account.yearChange();
+        int actual = account.yearChange();
+        int expected = 760;
 
         Assertions.assertEquals(expected, actual);
     }
