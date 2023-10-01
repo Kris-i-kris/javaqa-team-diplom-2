295e6875e1580a1a9f5c4b12427c27763c8c4e97
kris-i-kris <flare120491@gmail.com>
2023-10-01 19:09:39 +0700

    Tests3

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -20,16 +20,25 @@ public class SavingAccount extends Account {
      * @param maxBalance     - максимальный баланс
      * @param rate           - неотрицательное число, ставка в процентах годовых на остаток
      */
+
     public SavingAccount(int initialBalance, int minBalance, int maxBalance, int rate) {
-        if (rate < 0) {
-            throw new IllegalArgumentException(
-                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
-            );
-        }
+//        if (rate < 0) {
+//            throw new IllegalArgumentException(
+//                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
+//            );
+//        }
         this.balance = initialBalance;
         this.minBalance = minBalance;
         this.maxBalance = maxBalance;
         this.rate = rate;
+    }
+    public void setRate(int rate) {
+        Account acc = new Account();
+        if (acc.getRate() <= 0) {
+            throw new IllegalArgumentException(
+                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
+            );
+        }
     }
 
     /**
@@ -47,8 +56,9 @@ public class SavingAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = balance - amount;
-        if (balance > minBalance) {
+
+        if (balance + minBalance >= amount) {
+            balance = balance - amount;
             return true;
         } else {
             return false;
@@ -72,8 +82,8 @@ public class SavingAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        if (balance + amount < maxBalance) {
-            balance = amount;
+        if (balance + amount <= maxBalance) {
+            balance = balance + amount;
             return true;
         } else {
             return false;
