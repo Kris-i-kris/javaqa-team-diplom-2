e79b58f56d85235da2c9fa4b921a0de11c8056bc
Mirau Pavel <miraupavel@gmail.com>
2023-10-02 21:03:28 +0700

    del test

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -22,11 +22,11 @@ public class SavingAccount extends Account {
      */
 
     public SavingAccount(int initialBalance, int minBalance, int maxBalance, int rate) {
-//        if (rate < 0) {
-//            throw new IllegalArgumentException(
-//                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
-//            );
-//        }
+        if (rate < 0) {
+            throw new IllegalArgumentException(
+                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
+            );
+        }
         this.balance = initialBalance;
         this.minBalance = minBalance;
         this.maxBalance = maxBalance;
